191bb950e6d58e9620619b38f6f8f0cbd27da2d5
Midhul <midhul@Midhuls-MacBook-Pro.local>
2019-11-12 21:01:17 -0500

    Runner scripts

---
+++ examples/cpp/bench/run_server.sh
@@ -0,0 +1,5 @@
+y=$((NUM_CORES-1))
+service irqbalance stop
+set_irq_affinity_cpulist.sh 0-$y eno1
+show_irq_affinity.sh eno1
+perf record -o $OP_FILE -F 999 -C 0 -g -- GRPC_VERBOSITY=info GRPC_CORES_OVERRIDE=$NUM_CORES taskset -c 0-$y ./server -host 128.84.155.114 "$@"
