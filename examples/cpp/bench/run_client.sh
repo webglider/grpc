cfa5bc90b02554178dd7d38aab7b326f9f90f6b7
Midhul <midhul@Midhuls-MacBook-Pro.local>
2019-11-12 23:32:31 -0500

    Use eno1 IP address

--- examples/cpp/bench/run_client.sh
+++ examples/cpp/bench/run_client.sh
@@ -3,4 +3,4 @@ y=$((NUM_CORES-1))
 service irqbalance stop
 set_irq_affinity_cpulist.sh 0-$y eno1
 show_irq_affinity.sh eno1
-GRPC_VERBOSITY=info GRPC_CORES_OVERRIDE=$NUM_CORES perf record -o $OP_FILE -F 999 -C 0-$y -g -- taskset -c 0-$y ./client -server_host 128.84.155.114 "$@"
+GRPC_VERBOSITY=info GRPC_CORES_OVERRIDE=$NUM_CORES perf record -o $OP_FILE -F 999 -C 0-$y -g -- taskset -c 0-$y ./client -server_host 192.168.9.114 "$@"
